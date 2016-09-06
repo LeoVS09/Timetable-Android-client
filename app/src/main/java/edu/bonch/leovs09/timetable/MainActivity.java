37647f7afdcb761cfa2c4dd1f417b00d30f0fa72
LeoVS09 <LeoVS010@gmail.com>
2016-09-06 20:42:22 +0300

    edits

--- app/src/main/java/edu/bonch/leovs09/timetable/MainActivity.java
+++ app/src/main/java/edu/bonch/leovs09/timetable/MainActivity.java
@@ -21,7 +21,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
+import edu.bonch.leovs09.timetable.ODT.Week;
+import edu.bonch.leovs09.timetable.ODT.WeekBuilder;
 import edu.bonch.leovs09.timetable.REST.RestRequest;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,10 +38,13 @@ public class MainActivity extends AppCompatActivity {
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
+
     /**
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+
+    private Week week;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +61,11 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
+        try {
+            new HttpRequestSetCurrentWeek().execute();
+        }catch (Exception e){
+            Log.e("MainActivity", e.getMessage(), e);
+        }
 //--------------------------------button for update---------------------------------
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -99,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
-
+        private static final String ARG_SECTION_TEXT = "section_text";
         public PlaceholderFragment() {
         }
 
@@ -107,10 +118,11 @@ public class MainActivity extends AppCompatActivity {
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
+        public static PlaceholderFragment newInstance(int sectionNumber,String sectionText) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            args.putString(ARG_SECTION_TEXT, sectionText);
             fragment.setArguments(args);
             return fragment;
         }
@@ -121,43 +133,37 @@ public class MainActivity extends AppCompatActivity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            try {
-                new HttpRequestTask().execute(textView);
-            }catch (Exception e){
-                Log.e("textView: GetString",e.getMessage(),e);
-            }
+            textView.setText(getArguments().getString(ARG_SECTION_TEXT));
             return rootView;
         }
 
-        private class HttpRequestTask extends AsyncTask<TextView, Void, String> {
-
-
-            private RestRequest restRequest = new RestRequest();
-            TextView textView;
-            @Override
-            protected String doInBackground(TextView... params) {
-                this.textView = params[0];
-                try {
-
-                    RestRequest rest = new RestRequest();
-                    //rest.in  определят что отсылка произойдет по адресу http://{host}/message/{username} и возврашает rest
-                    //rest.send отсылает обьект params[0] и возврашает обьект типа Message.class
-                    return restRequest.in("currentTimeTable", "group").GetObjAndStatus(String.class).toString();
-                } catch (Exception e) {
-                    Log.e("MainActivity", e.getMessage(), e);
-                }
-
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(String message) {
-                textView.setText(message);
-            }
-
-        }
     }
+    private class HttpRequestSetCurrentWeek extends AsyncTask<Void, Void, String> {
 
+        private RestRequest restRequest = new RestRequest();
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+
+                RestRequest rest = new RestRequest();
+                return restRequest.in("currentTimeTable", "group").GetObjAndStatus(String.class).toString();
+            } catch (Exception e) {
+                Log.e("HttpRequest::Start", e.getMessage(), e);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            try {
+                week = new WeekBuilder().buildWeek(response);
+            }catch (Exception e){
+                Log.e("HttpRequest::OnPost",e.getMessage(),e);
+            }
+        }
+
+    }
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
@@ -172,7 +178,7 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            return PlaceholderFragment.newInstance(position + 1,week.getDays().get(position).getName());
         }
 
         @Override
